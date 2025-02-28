1306dd03697a43c40265978860416951cc19c8cd
Orwellian <orwellian225@gmail.com>
2025-02-28 19:31:13 +0200

    add: machine menu

---
+++ src/lib/components/MachineMenu.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { getContext } from "svelte";
+
+	import { PencilSimple } from "phosphor-svelte";
+	import { Separator } from "bits-ui";
+
+	import TuringMachine from "$lib/tm-engine/tm-machine.svelte";
+
+    let current_turing_machine: TuringMachine = getContext("current_turing_machine");
+    let show = $state(false);
+</script>
+
+<div>
+	<div class="w-full flex justify-between items-center">
+	    <h2 class="text-2xl">{current_turing_machine.identifier}</h2>
+	    <span class="flex justify-evenly items-center gap-[1px]">
+	        <button class="border-[1px] p-1 border-black hover:bg-zinc-100" onclick={() => {
+	        }}><PencilSimple size={20}/></button>
+	    </span>
+	</div>
+
+	<Separator.Root class="bg-black my-2 data-[orientation=horizontal]:h-[2px] data-[orientation=horizontal]:w-full" />
+</div>
