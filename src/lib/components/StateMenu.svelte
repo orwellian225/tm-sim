657db500dc0b81150f2c811e36e48c48ebcf768d
Orwellian <orwellian225@gmail.com>
2025-03-07 11:32:11 +0200

    add: tooltip overlay for state modifier

--- src/lib/components/StateMenu.svelte
+++ src/lib/components/StateMenu.svelte
@@ -2,7 +2,7 @@
     import { getContext } from "svelte";
 
     import { Plus, PencilSimple, TrashSimple, EyeClosed, Eye, Check, X } from "phosphor-svelte";
-    import { Separator } from "bits-ui";
+    import { Separator, Tooltip } from "bits-ui";
 	import RecursiveTextMenu from "./RecursiveTextMenu.svelte";
 
 	import type TMFile from "$lib/tm-engine/tm-file.svelte";
@@ -62,25 +62,39 @@
                     {#if editing_state_idx != idx}
                         <p>* {state}</p>
                         <span class="flex justify-evenly items-center gap-[1px] pr-1">
-                            <button class="border-[1px] w-[26px] h-[26px] p-1 border-black hover:bg-zinc-100 flex items-center justify-center" onclick={() => {
-                                if (modify_state_type == idx)
-                                    modify_state_type = -1;
-                                else
-                                    modify_state_type = idx;
-                            }}>
-                                {#if idx == current_turing_machine.machine.initial_state}
-                                    <div class="w-full h-full bg-blue-500"></div>
-                                {/if}
-                                {#if idx == current_turing_machine.machine.accept_state}
-                                    <div class="w-full h-full bg-green-500"></div>
-                                {/if}
-                                {#if idx == current_turing_machine.machine.reject_state}
-                                    <div class="w-full h-full bg-red-500"></div>
-                                {/if}
-                                {#if idx != current_turing_machine.machine.initial_state && idx != current_turing_machine.machine.accept_state && idx != current_turing_machine.machine.reject_state}
-                                    >
-                                {/if}
-                            </button>
+                        	<Tooltip.Provider><Tooltip.Root>
+
+	                            <Tooltip.Trigger delayDuration={200}><button class="border-[1px] w-[26px] h-[26px] p-1 border-black hover:bg-zinc-100 flex items-center justify-center" onclick={() => {
+	                                if (modify_state_type == idx)
+	                                    modify_state_type = -1;
+	                                else
+	                                    modify_state_type = idx;
+	                            }}>
+	                                {#if idx == current_turing_machine.machine.initial_state}
+	                                    <div class="w-full h-full bg-blue-500"></div>
+	                                {/if}
+	                                {#if idx == current_turing_machine.machine.accept_state}
+	                                    <div class="w-full h-full bg-green-500"></div>
+	                                {/if}
+	                                {#if idx == current_turing_machine.machine.reject_state}
+	                                    <div class="w-full h-full bg-red-500"></div>
+	                                {/if}
+	                                {#if idx != current_turing_machine.machine.initial_state && idx != current_turing_machine.machine.accept_state && idx != current_turing_machine.machine.reject_state}
+	                                    >
+	                                {/if}
+	                            </button></Tooltip.Trigger>
+								<Tooltip.Content><div class="p-1 w-fit flex flex-col justify-start items-center bg-white border-black border-[1px]">
+							        {#if idx == current_turing_machine.machine.initial_state}
+										<p>Initial State</p>
+							        {/if}
+							        {#if idx == current_turing_machine.machine.accept_state}
+										<p>Accept State</p>
+							        {/if}
+							        {#if idx == current_turing_machine.machine.reject_state}
+										<p>Reject State</p>
+							        {/if}
+								</div></Tooltip.Content>
+							</Tooltip.Root></Tooltip.Provider>
                             <button class="border-[1px] p-1 border-black hover:bg-zinc-100" onclick={() => {
                                 editing_state_idx = idx;
                                 editing_state_value = state;
