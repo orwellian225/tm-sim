b866647e26cf62a30c7c70fb7026341d23fbcb8e
Orwellian <orwellian225@gmail.com>
2025-03-09 22:59:51 +0200

    fix: updating transitions doesn't update diagram

--- src/lib/components/panel-views/TransitionTablePanel.svelte
+++ src/lib/components/panel-views/TransitionTablePanel.svelte
@@ -5,58 +5,67 @@
 
 	import { TrashSimple } from 'phosphor-svelte';
 	import { Currency } from 'lucide-svelte';
+	import DiagramState from '$lib/tm-engine/tm-diagram';
 
-	let current_turing_machine: TMFile = getContext('current_turing_machine');
+	let current_tm: TMFile = getContext('current_turing_machine');
 </script>
 
 <ul class="space-y-2 p-1">
-	{#each current_turing_machine.machine.states as state, state_idx}
-		{#each current_turing_machine.machine.alphabet as symbol, symbol_idx}
-			{@const transition: TMTransition | null = current_turing_machine.machine.find_transition(state_idx, symbol_idx)}
-			{#if state_idx != current_turing_machine.machine.accept_state && state_idx != current_turing_machine.machine.reject_state}
-				<li class="w-fit flex flex-row items-center justify-center gap-2 h-8">
-					{state}
-					{symbol}
-					->
-					{#if transition !== null}
-						<select bind:value={transition.to_state} class="p-1">
-							{#each current_turing_machine.machine.states as state, idx}
-								<option value={idx}>{state}</option>
-							{/each}
-						</select>
-						<select bind:value={transition.write_symbol} class="p-1">
-							{#each current_turing_machine.machine.alphabet as symbol, idx}
-								<option value={idx}>{symbol}</option>
-							{/each}
-						</select>
-						<select bind:value={transition.direction} class="p-1">
-							<option value={-1}>Left</option>
-							<option value={1}>Right</option>
-						</select>
-						<button
-							class="border-[1px] p-1 border-black hover:bg-zinc-100"
-							onclick={() => {
-								transition.to_state = null;
-								transition.write_symbol = null;
-								transition.direction = null;
-							}}><TrashSimple size={18} /></button
-						>
-					{:else}
-						<button
-							class="border-[1px] p-1 border-black hover:bg-zinc-100"
-							onclick={() => {
-								current_turing_machine.machine.transitions.push({
-									from_state: state_idx,
-									read_symbol: symbol_idx,
-									to_state: 0,
-									write_symbol: 0,
-									direction: 1
-								});
-							}}>No Transition</button
-						>
-					{/if}
-				</li>
-			{/if}
-		{/each}
+	{#each current_tm.machine.transitions as transition, idx}
+		{#if transition.from_state != current_tm.machine.accept_state && transition.from_state != current_tm.machine.reject_state}
+			<li class="w-fit flex flex-row items-center justify-center gap-2 h-8">
+				{current_tm.machine.states[transition.from_state]},
+				{current_tm.machine.states[transition.read_symbol]}
+				->
+				{#if transition.to_state !== null}
+					<select bind:value={transition.to_state} onchange={() => {
+						current_tm.diagram.transitions[idx].terminal_point = { 
+							state_position: current_tm.diagram.states[transition.to_state].position,
+							radius: DiagramState.radius + 5,
+							angle: 0
+						};
+						current_tm.diagram.transitions[idx].refresh_angles();
+					}} class="p-1">
+						{#each current_tm.machine.states as state, idx}
+							<option value={idx}>{state}</option>
+						{/each}
+					</select>
+					<select bind:value={transition.write_symbol} class="p-1">
+						{#each current_tm.machine.alphabet as symbol, idx}
+							<option value={idx}>{symbol}</option>
+						{/each}
+					</select>
+					<select bind:value={transition.direction} class="p-1">
+						<option value={-1}>Left</option>
+						<option value={1}>Right</option>
+					</select>
+					<button
+						class="border-[1px] p-1 border-black hover:bg-zinc-100"
+						onclick={() => {
+							transition.to_state = null;
+							transition.write_symbol = null;
+							transition.direction = null;
+							current_tm.diagram.transitions[idx].terminal_point = null;
+							current_tm.diagram.transitions[idx].refresh_angles();
+						}}><TrashSimple size={18} /></button
+					>
+				{:else}
+					<button
+						class="border-[1px] p-1 border-black hover:bg-zinc-100"
+						onclick={() => {
+							transition.to_state = transition.from_state;
+							transition.write_symbol = transition.read_symbol;
+							transition.direction = 1;
+							current_tm.diagram.transitions[idx].terminal_point = { 
+								state_position: current_tm.diagram.states[transition.to_state].position,
+								radius: DiagramState.radius + 5,
+								angle: 0
+							};
+							current_tm.diagram.transitions[idx].refresh_angles();
+						}}>No Transition</button
+					>
+				{/if}
+			</li>
+		{/if}
 	{/each}
 </ul>
